dd04790c03fa179a3bd98adc5b2ce6993e02edce
Adarsha Balaji <abalaji@polaris-login-02.hsn.cm.polaris.alcf.anl.gov>
2025-05-19 19:45:30 +0000

    logging quant to csv

--- scripts/eval/eval_l1_quant.py
+++ scripts/eval/eval_l1_quant.py
@@ -14,10 +14,22 @@ from verl.utils.model import update_model_config
 from vllm import SamplingParams
 from llama_cpp import Llama
 
+import torch
 from datasets import load_dataset, Dataset
 import numpy as np
 
+#DEBUG FLAGS:
+TEST=0
 LLAMA=0
+
+import csv
+
+fieldnames=['Query', 'Response']
+def write_dict_to_csv(data, filename, fieldnames):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
 
 class Safe_SubString:
     def __init__(self) -> None:
@@ -89,16 +101,25 @@ class Safe_SubString:
         refusal_state = [s in response  for s in self.refusal_string]
         return not any(refusal_state)
 
-#DEBUG FLAGS:
-TEST=0
 
 model_name = "mradermacher/L1-Qwen-1.5B-Exact-GGUF"
 filename = "L1-Qwen-1.5B-Exact.f16.gguf"
-if 1:
+
+if TEST:
+    input_text = "What are we having for dinner?"
+    input_ids = tokenizer(input_text, return_tensors="pt", device_map='auto')
+    output = model.generate(**input_ids, max_new_tokens=10)
+    print(tokenizer.decode(output[0], skip_special_tokens=True))
+    exit()
+
+if TEST==0:    
     config = AutoConfig.from_pretrained(model_name)
     actor_model_config = AutoConfig.from_pretrained(model_name, gguf_file=filename)
-    model = AutoModelForCausalLM.from_pretrained(model_name, gguf_file="L1-Qwen-1.5B-Exact.Q8_0.gguf", model_type="llama", device_map = 'auto')
-    tokenizer = AutoTokenizer.from_pretrained(model_name, gguf_file=filename)
+    model = AutoModelForCausalLM.from_pretrained(model_name, gguf_file="L1-Qwen-1.5B-Exact.Q8_0.gguf", torch_dtype=torch.float16, model_type="llama", device_map = 'auto')
+    tokenizer = AutoTokenizer.from_pretrained(model_name, gguf_file=filename, device_map='auto')
+    
+    num_gpus = torch.cuda.device_count()
+    print(f"Number of GPUs available: {num_gpus}")
 
 #tokenizer.pad_token = tokenizer.eos_token
 dataset = pd.read_parquet('/lus/eagle/projects/argonne_tpc/abalaji/datasets/deepscaler/data_1024/strongreject.parquet')
@@ -118,16 +139,11 @@ evaluator_strongreject = Safe_SubString()
 output_lst = []
 score_lst = []
 passes=0
-if TEST:
-    input_text = "What are we having for dinner?"
-    input_ids = tokenizer(input_text, return_tensors="pt", device_map='auto')
-    output = model.generate(**input_ids, max_new_tokens=10)
-    print(tokenizer.decode(output[0], skip_special_tokens=True))
-    exit()
+
+output_dict = {'prompt': [], 'response': []}
 
 for i,p in enumerate(chat_lst):
     #print(f'[{batch_idx+1}/{num_batch}] Start to process.')
-    #batch_chat_lst = chat_lst[batch_idx * batch_size:(batch_idx + 1) * batch_size]
     prompt = p
     
     #input_ids = tokenizer(prompt, return_tensors="pt").to("cuda")
@@ -138,17 +154,24 @@ for i,p in enumerate(chat_lst):
                                          max_length=1536,
                                          return_tensors='pt',
                                          return_dict=True,
+                                         device_map='auto',
                                          tokenize=True)
    
-    inputs.to("cuda")
-    input_ids = inputs['input_ids']
-    input_len = len(input_ids)
+    inputs = inputs.to("cuda")
+    #input_ids = inputs['input_ids'].to('cuda')
+    #input_len = len(input_ids)
     # Generate all samples at once
     output = model.generate(**inputs, max_new_tokens=1536)
     output_text = tokenizer.decode(output[0], skip_special_tokens=False)
 
+    print('Prompt: %s' %(prompt))
+    print('Response: %s' %(output_text))
+
+    output_dict['prompt'].append(prompt)
+    output_dict['response'].append(output_text)
+
     score = evaluator_strongreject.eval_single(output_text)
-     
+    
     if score==0:
         passes+=1
     
@@ -159,17 +182,16 @@ for i,p in enumerate(chat_lst):
 pass_at_1 = passes/total_samples
 print(pass_at_1)
 
-#    if LLAMA:
-#        dataset = Dataset.from_dict({"forbidden_prompt": 2*[prompt], "response":[output_text]})  
-#        EVALUATOR = ["llama_guard"]
-#        eval_dataset = evaluate_dataset(dataset, EVALUATOR)
-#        print(eval_dataset["score"])
-#        eval_dataset.to_parquet("eval_results.parquet")
+filename = 'l1_quant_response.csv'
 
-# to calculate the top_p
-    # Repeat the batch n_samples times
-    #repeated_chat_lst = []
-    #for chat in batch_chat_lst:
-     #   repeated_chat_lst.extend([chat] * n_samples)
+write_dict_to_csv(output_dict, filename, fieldnames)
+
+
+if LLAMA:
+    dataset = Dataset.from_dict({"forbidden_prompt": 2*[prompt], "response":[output_text]})  
+    EVALUATOR = ["llama_guard"]
+    eval_dataset = evaluate_dataset(dataset, EVALUATOR)
+    print(eval_dataset["score"])
+    eval_dataset.to_parquet("eval_results.parquet")
 
 
